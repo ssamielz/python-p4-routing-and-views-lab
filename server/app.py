529a25f3507a09c69adb7b5ebcfd24ab4739f4a3
Sam <ssamsanya3@gmail.com>
2025-07-03 17:18:40 +0300

    routing views

--- server/app.py
+++ server/app.py
@@ -4,5 +4,38 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return f'{param}'
+
+@app.route('/count/<int:param>')
+def count(param):
+    return '\n'.join(str(n) for n in range(param)) + '\n'
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    result = None
+    if operation =='+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 == 0:
+            return 'Error: Division by zero'
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return 'Error: Unsupported operation'
+
+    return str(result)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
